37958b2c4f25f859e6f5b5b03c3b1adc2bc4983c
Aleksey Tyutrin <atutrin@gmail.com>
2018-02-15 16:02:07 +0800

    + CRUD

---
+++ common/models/City.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $region_id
+ *
+ * @property Region $region
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['region_id'], 'required'],
+            [['region_id'], 'integer'],
+            [['name'], 'string', 'max' => 256],
+            [['region_id'], 'exist', 'skipOnError' => true, 'targetClass' => Region::className(), 'targetAttribute' => ['region_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'region_id' => 'Region ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRegion()
+    {
+        return $this->hasOne(Region::className(), ['id' => 'region_id']);
+    }
+}
